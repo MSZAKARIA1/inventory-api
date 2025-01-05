3cdcb03df48bb4f37f52c0cad7c0eeb2486e7942
mahama saani Zakaria <zakariamahamasaani@gmail.com>
2025-01-05 15:01:45 +0000

    updated settings allowes hots

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-r6s!d4%9subf!4z*pvrct_*93+gtq5!kirb**fdh4^rawt_yyf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["inventory-api", ".herokuapp.com"]
 
 
 # Application definition
